d0be02cf06d23af45f976220499e4d8297c09d33
Hafizh ZR <hafizhzaghlul882@gmail.com>
2023-05-23 19:15:09 +0700

    packagejson

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2cb139af4405e79d"; }
+/******/ 		__webpack_require__.h = function() { return "8a2872494194c999"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
