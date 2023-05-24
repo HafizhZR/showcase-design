ca2c17eef4bd262cc4aeb17c563de6e5d5569a25
Harits Naufal <haritsnaufalichs11@gmail.com>
2023-05-24 22:57:47 +0700

    Navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a2872494194c999"; }
+/******/ 		__webpack_require__.h = function() { return "a9da8d7f6aef0548"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Navbar from "@/components/navbar/Navbar"
+
 export default function Home() {
   return (
- <h1>halo</h1>
+    <>
+      <Navbar />
+      <div className='h-screen flex justify-center items-center bg-slate-300'>
+        <h1 className='text-3xl'>Hello World</h1>
+      </div>
+    </>
   )
 }
