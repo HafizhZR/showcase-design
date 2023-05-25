4c3f4e7a1ff7ea75a6c4d781b742ff3628a3e24e
T.M. Roy Adrian <tmroyadrian098@gmail.com>
2023-05-25 16:09:45 +0700

    Create Homepage views and add some images asset to use for homepage
    homepage belum responsive

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a2872494194c999"; }
+/******/ 		__webpack_require__.h = function() { return "482174faa3424b78"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
