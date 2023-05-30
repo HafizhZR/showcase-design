3f93455b7b040b763ce32dd161239b77f41d86d8
Harits Naufal <haritsnaufalichs11@gmail.com>
2023-05-31 04:53:12 +0700

    Fix Nav, Add Detail Post, Add User Profile, & Add Edit Profile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import Navbar from "@/components/navbar/Navbar"
-import Footer from "@/components/footer/Footer"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className='h-screen flex justify-center items-center bg-slate-300'>
-        <h1 className='text-3xl'>Hello World</h1>
+      <div className='min-h-screen flex flex-col py-8 px-8'>
+        <div className="post-detail">
+          <Link href="/posts/detail">
+            <h1 className="text-xl hover:text-primary hover:underline">Test Detail Postingan</h1>
+          </Link>
+        </div>
       </div>
       <Footer />
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,6 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    screens: {
-      'sm': {'max': '639px'},
-      'md': {'min': '639px'},
-    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -17,10 +13,10 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: "#2A9D8F",
-        secondary: "#004EC4",
+        primary: '#2A9D8F',
+        secondary: '#004EC4',
       },
     },
   },
   plugins: [],
-}
+};
