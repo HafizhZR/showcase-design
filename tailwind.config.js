9a96acff2465e94baeab3a579c8bde6247db70a9
HafizhZR <hafizhzaghlul882@gmail.com>
2023-05-31 17:45:22 +0700

    fix tailwind config screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': {'max': '639px'},
+      'md': {'min': '639px'},
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -19,4 +23,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
